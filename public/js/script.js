4326737e446d9fa05d26c97ce6f95ac884f08c90
Alexanderzx0o <esdrash981@gmail.com>
2025-10-15 15:33:04 -0600

    Simplify script by adding welcome alert
    Removed DOMContentLoaded event listener and added a welcome alert.

--- public/js/script.js
+++ public/js/script.js
@@ -1,13 +1,1 @@
-
-document.addEventListener("DOMContentLoaded", () => {
-  
-  const botones = document.querySelectorAll("button");
-
- 
-  botones.forEach(boton => {
-    boton.addEventListener("click", () => {
-      alert("¡Gracias por tu interés!  Pronto podrás adquirir nuestras fragancias.");
-    });
-  });
-});
-
+alert("Bienvenido");
